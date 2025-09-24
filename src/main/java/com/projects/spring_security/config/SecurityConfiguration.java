5434d933c751d26054bab54d174c5895129159b1
Aqil Səidov <aqilasa638@gmail.com>
2025-09-24 20:58:43 +0400

    Default Security Filter Chain overviewed

---
+++ src/main/java/com/projects/spring_security/config/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.projects.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
---
+++ src/main/java/com/projects/spring_security/controller/BalanceController.java
@@ -0,0 +1,12 @@
+package com.projects.spring_security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    @GetMapping("/myBalance")
+    public String myBalance() {
+        return "Balance Information";
+    }
+}
