45022b72085f2b0c0db81bb5465e4489ca3440f2
Aqil Səidov <aqilasa638@gmail.com>
2025-09-25 01:00:21 +0400

    Multiple users created with InMemoryUserDetailsManager and UserDetailsService

--- src/main/java/com/projects/spring_security/config/SecurityConfiguration.java
+++ src/main/java/com/projects/spring_security/config/SecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.projects.spring_security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +23,26 @@ public class SecurityConfiguration {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetails() {
+        UserDetails user1 = User.builder()
+                .username("aqil")
+                .password("{noop}1234")
+                .roles("USER").build();
+
+        UserDetails user2 = User.builder()
+                .username("max")
+                .password("{noop}54321")
+                .roles("USER").build();
+
+        UserDetails user3 = User.builder()
+                .username("alex")
+                .password("{noop}12345")
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user1,user2,user3);
+
     }
 }
