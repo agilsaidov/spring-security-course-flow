c3eddcc27deb14e80c6dc3e84f2c3569b3795eda
Aqil Səidov <aqilasa638@gmail.com>
2025-09-25 22:46:24 +0400

    User Entity class implemented

---
+++ src/main/java/com/projects/spring_security/model/User.java
@@ -0,0 +1,24 @@
+package com.projects.spring_security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Table(name = "SecurityTestUsers")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+}
