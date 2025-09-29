6e82499f84a4dbaf92a8fad77bab5b462c219073
Aqil Səidov <aqilasa638@gmail.com>
2025-09-29 12:08:17 +0400

    Create ProdSecurityConfiguration which works with HTTPS protocol

--- src/main/java/com/projects/spring_security/config/SecurityConfiguration.java
+++ src/main/java/com/projects/spring_security/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.projects.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+@Profile("!prod")
 @Configuration
 public class SecurityConfiguration {
 
