8aae666749f7f44dca1cdf517cebc918a410c8d5
Aqil Səidov <aqilasa638@gmail.com>
2025-09-29 12:07:00 +0400

    Update .gitignore

---
+++ src/main/java/com/projects/spring_security/config/ProdSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.projects.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.redirectToHttps(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.
+                authorizeHttpRequests(request -> request.
+                        requestMatchers("/myAccount","/myBalance","/myCards").authenticated().
+                        requestMatchers("/notifications","/register").permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+
+}
